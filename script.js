f254a336c4bf77bc9c4b7db3137e41a71c8783a5
EddyKubwimana <eddy.kubwimana@ashesi.edu.gh>
2025-06-25 09:48:43 +0000

    updating the the grade for my sweetie

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 
 
   // Static grade value (update this number to change the grade)
-const grade = 81; // Change this value to update the grade
+const grade = 99.95; // Change this value to update the grade
 
 // Function to update the grade display
 function updateGradeDisplay(grade) {
